470ba91b9f02e2cc827b70ba33396c5aa0bc25be
Patrick Santos <patrickbarnabe@github.com>
2020-09-09 15:39:09 -0300

    2.3.1

--- app/Controllers/Http/FuncionarioController.js
+++ app/Controllers/Http/FuncionarioController.js
@@ -1,12 +1,5 @@
 'use strict'
 
-const Database = use("Database")
-const Funcionario = use('App/Models/Funcionario')
-const User = use('App/Models/User')
-const Escola = use('App/Models/Escola')
-const Endereco = use('App/Models/endereco')
-const Pessoa =  use('App/Models/Pessoa')
-const { validateAll } = use('Validator')
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
@@ -14,40 +7,20 @@ const { validateAll } = use('Validator')
 /**
  * Resourceful controller for interacting with funcionarios
  */
+
+const Database = use("Database")
+const Funcionario = use('App/Models/Funcionario')
+const User = use('App/Models/User')
+const Escola = use('App/Models/Escola')
+const Endereco = use('App/Models/endereco')
+const Pessoa =  use('App/Models/Pessoa')
+const { validateAll } = use('Validator')
+
 class FuncionarioController {
-  /**
-   * Show a list of all funcionarios.
-   * GET funcionarios
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
+
   async index ({ request, response, view }) {
   }
 
-  /**
-   * Render a form to be used for creating a new funcionario.
-   * GET funcionarios/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {    
-    
-  }
-
-  /**
-   * Create/save a new funcionario.
-   * POST funcionarios
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async store ({ request, response }) {
     try{
       const trx = await Database.beginTransaction()
@@ -129,54 +102,18 @@ class FuncionarioController {
    }
   }
 
-  /**
-   * Display a single funcionario.
-   * GET funcionarios/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
   async show ({ params, request, response, view, auth }) {
     const tarefa = Database.select("ativo").from("funcionarios").where("user_id",auth.user.id)
     return tarefa
 
   }
 
-  /**
-   * Render a form to update an existing funcionario.
-   * GET funcionarios/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update funcionario details.
-   * PUT or PATCH funcionarios/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async update ({ params, request, response }) {
   }
 
-  /**
-   * Delete a funcionario with id.
-   * DELETE funcionarios/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async destroy ({ params, request, response }) {
   }
+
 }
 
 module.exports = FuncionarioController
